ee1d61290dcfe993aa0d036979f1beab2866c3ac
jiwooproity <jiwooproity@naver.com>
2023-06-15 00:55:29 +0900

    chore: less, less-loader 모듈 추가 및 less -> css 컴파일 설정 추가
    .less 파일을 .css 형식으로 컴파일하여 처리하기 위한 less, less-loader 모듈 추가 및 설정 적용

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -7,38 +7,42 @@ const TerserPlugin = require("terser-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
-    mode: "production",
-    devtool: "cheap-module-source-map",
-    output: {
-        filename: "bundle.[contenthash].js",
-        path: path.resolve(__dirname, "../build"),
-        publicPath: "./",
-        clean: true,
+  mode: "production",
+  devtool: "cheap-module-source-map",
+  output: {
+    filename: "bundle.[contenthash].js",
+    path: path.resolve(__dirname, "../build"),
+    publicPath: "./",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.less$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  optimization: {
+    usedExports: true,
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+      new CssMinimizerPlugin(),
+    ],
+    splitChunks: {
+      chunks: "all",
     },
-    module: {
-        rules: [
-            {
-                test: /\.(sa|sc|c)ss$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-            }
-        ]
-    },
-    plugins: [new MiniCssExtractPlugin()],
-    optimization: {
-        usedExports: true,
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                terserOptions: {
-                    compress: {
-                        drop_console: true,
-                    }
-                }
-            }),
-            new CssMinimizerPlugin(),
-        ],
-        splitChunks: {
-            chunks: "all",
-        }
-    }
-})
+  },
+});
