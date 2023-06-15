d211d9c40fb075d57cf5b968a3dac5bf543cbcec
jiwooproity <jiwooproity@naver.com>
2023-06-16 01:22:28 +0900

    feat: 다국어 변경을 위한 i18n 활성화 작업 및 메뉴 아이템 다국어 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "@/i18n";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container as Element);
 
 root.render(<App />);
