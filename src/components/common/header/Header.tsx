eb68004a108de45b44e377cbde23d1a965cd421f
jiwooproity <jiwooproity@naver.com>
2023-06-16 01:20:39 +0900

    feat: 다국어 변경 기능 추가 - i18n useTranslation 함수를 통한 i18n.language 다국어 변경 기능 추가 건 - 버튼 컴포넌트 적용 및 다국어 변경 버튼 스타일 추가

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 import "./header.less";
 
 import { menu } from "@/interface"; // 메뉴 목록
+import { Button } from "@/components/common";
 
 const Logo = () => {
   return (
@@ -13,14 +15,40 @@ const Logo = () => {
 };
 
 const Menu = () => {
+  const { t } = useTranslation();
+
   return (
     <ul className="nav-menu">
       {menu.map((item) => (
         <li key={item.id} className="nav-menu-item">
-          <Link to={item.to}>{item.label}</Link>
+          <Link to={item.to}>{t(`${item.label}`)}</Link>
         </li>
       ))}
     </ul>
+  );
+};
+
+const TranslationBtn = () => {
+  const { i18n } = useTranslation();
+  const { resources } = i18n.options;
+
+  const onChangeLang = (e: { target: EventTarget }) => {
+    const { value } = e.target as HTMLButtonElement;
+    i18n.changeLanguage(value);
+  };
+
+  return (
+    <div className="nav-translation-area">
+      {Object.keys(resources).map((lang) => (
+        <Button
+          className="nav-translation-btn"
+          value={lang}
+          onClick={onChangeLang}
+        >
+          {lang}
+        </Button>
+      ))}
+    </div>
   );
 };
 
@@ -28,8 +56,13 @@ const Header = () => {
   return (
     <nav id="navbar" className="nav-container">
       <div className="nav-wrapper">
-        <Logo />
-        <Menu />
+        <div className="nav-left-side">
+          <Logo />
+          <Menu />
+        </div>
+        <div className="nav-right-side">
+          <TranslationBtn />
+        </div>
       </div>
     </nav>
   );
