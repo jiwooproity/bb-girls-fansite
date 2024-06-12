b5cef82a714df1577949cc4c04c7fdbec3fc1965
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-06-12 17:36:39 +0900

    Feat: Favicon 적용 및 Aurora Canvas 로직 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
 import '@/shared/styles/globals.css';
+import { Metadata } from 'next';
 
 import { Header } from '@/widgets';
 import { Aurora } from '@/shared/components';
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'BBGirls | Fansite',
   description: '브레이브걸스 팬사이트',
+  icons: {
+    icon: '/bbgirls-logo.ico',
+  },
 };
 
 export default function RootLayout({
